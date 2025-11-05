dc735829daa08151caaa2529ccda35ccb8c3f161
Taral Shah <taralshah992005@gmail.com>
2025-11-05 13:55:28 +0530

    update: Leaderboard updated [5/11]

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -115,7 +115,7 @@ function Leaderboard() {
         <h1>🏆 Leaderboard</h1>
         <p className="subtitle">Track your progress and compete with peers</p>
         <p className="last-update">
-          Last updated: 04/11/2025 13:05 PM
+          Last updated: 05/11/2025 13:55 PM
         </p>
       </div>
 
